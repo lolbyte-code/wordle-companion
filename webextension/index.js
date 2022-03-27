56ff552042ffb291a65e80bfd02aa69b8e324f3d
LolByte <crxlolbyte@gmail.com>
2022-03-27 10:36:02 -0700

    Fix next/prev actions

--- webextension/index.js
+++ webextension/index.js
@@ -120,7 +120,7 @@ const suggestWord = (direction) => {
       suggestionIdx = guesses.length + suggestionIdx
     }
     if (suggestionIdx >= guesses.length) {
-      suggestionIdx = guesses.length - suggestionIdx
+      suggestionIdx = suggestionIdx - guesses.length
     }
     document.getElementById("board").removeChild(suggestionElement)
     // TODO: impossible guess?
@@ -130,6 +130,7 @@ const suggestWord = (direction) => {
 
 const tryWord = () => {
   if (solved) return
+  suggestionIdx = 0
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { type: "try", word: suggestion })
   })
